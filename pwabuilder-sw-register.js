45d924a262e42474cf6171d1a0731a58f5e2ffb1
Jonathan Chin <chin_jonathan@hotmail.com>
2018-03-23 21:36:32 -0400

    add a offline.html service worker

---
+++ pwabuilder-sw-register.js
@@ -0,0 +1,13 @@
+// this is the "Offline page" service worker
+
+// add this below the content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  // register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js', {
+    scope: './'
+  }).then(function (reg) {
+    console.log('Service worker has been registered for scope:' + reg.scope);
+  });
+}
