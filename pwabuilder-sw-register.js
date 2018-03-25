07e798d87a033f483e9f14ec66c3754a7426163f
Jonathan Chin <chin_jonathan@hotmail.com>
2018-03-24 21:23:21 -0400

    use a better service worker

--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -1,13 +1,3 @@
-// this is the "Offline page" service worker
-
-// add this below the content to your HTML page, or add the js file to your page at the very top to register service worker
-if (navigator.serviceWorker.controller) {
-  console.log('[PWA Builder] active service worker found, no need to register')
-} else {
-  // register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js', {
-    scope: './'
-  }).then(function (reg) {
-    console.log('Service worker has been registered for scope:' + reg.scope);
-  });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('pwabuilder-sw.js');
 }
